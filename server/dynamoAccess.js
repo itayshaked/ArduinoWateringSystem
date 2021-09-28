b4ea5c91343b00e5defe13edde5fddce10c296d3
Itay <itaysh3r@gmail.com>
2021-09-28 15:26:36 +0300

    process env

--- server/dynamoAccess.js
+++ server/dynamoAccess.js
@@ -2,9 +2,9 @@ import AWS from 'aws-sdk'
 
 
 AWS.config.update({
-    "region":AWS_DEFAULT_REGION,
-   "accessKeyId":AWS_ACCESS_KEY_ID,
-    "secretAccessKey":AWS_SECRET_ACCESS_KEY_ID
+    "region":process.env.AWS_DEFAULT_REGION,
+   "accessKeyId":process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey":process.env.AWS_SECRET_ACCESS_KEY_ID
 })
 const dynamoCLient=new AWS.DynamoDB.DocumentClient()
 const TABLE_NAME="Watering_System_Data"
@@ -60,7 +60,7 @@ app.post('/sensors',async(req,res)=>{
             ,
             "timestamp":parseInt(req.body.timestamp)
             ,
-            
+
             "temp":parseInt(req.body.temp)
             ,
             "UV":parseInt(req.body.UV)
